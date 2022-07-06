4e062b061871cf5a57c7560e6fbe277f266ee590
jfernandezsw <83318802+jfernandezsw@users.noreply.github.com>
2022-07-06 09:08:55 -0300

    APPS-4594 Added validation before adding a new message. (#9)
    * APPS-4594 Added validation before adding a new message.

--- tests/SprykerSdkTest/AsyncApi/Console/SchemaMessageAddConsoleTest.php
+++ tests/SprykerSdkTest/AsyncApi/Console/SchemaMessageAddConsoleTest.php
@@ -14,6 +14,7 @@ use SprykerSdk\AsyncApi\Exception\InvalidConfigurationException;
 use SprykerSdk\AsyncApi\Message\AsyncApiError;
 use SprykerSdkTest\AsyncApi\AsyncApiTester;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 use Transfer\AsyncApiBuilderTestTransfer;
 
 /**
@@ -99,5 +100,70 @@ class SchemaMessageAddConsoleTest extends Unit
             ],
             ['verbosity' => OutputInterface::VERBOSITY_VERBOSE],
         );
+    }
+
+    /**
+     * This test ensures that a message can only be added once per channel.
+     * The given API file already has the message that should be added and thus adding it again will be skipped.
+     *
+     * @return void
+     */
+    public function testAddMessageOnlyWhenMessageNameDoesNotExistInChannel(): void
+    {
+        // Arrange
+        $commandTester = $this->tester->getConsoleTester(SchemaMessageAddConsole::class, false);
+
+        // Act
+        // The message 'OutgoingMessage' is added for the second time in the channel 'channelNameA'
+        $commandTester->execute(
+            [
+                '--' . SchemaMessageAddConsole::OPTION_MESSAGE_TYPE => 'subscribe',
+                '--' . SchemaMessageAddConsole::OPTION_PROPERTY => ['property:string'],
+                '--' . SchemaMessageAddConsole::OPTION_ASYNC_API_FILE => codecept_data_dir('api/asyncapi/asyncapi.yml'),
+                SchemaMessageAddConsole::ARGUMENT_CHANNEL_NAME => 'channelNameA',
+                SchemaMessageAddConsole::ARGUMENT_OPERATION_ID => 'operationId',
+                SchemaMessageAddConsole::ARGUMENT_MESSAGE_NAME => 'OutgoingMessage',
+            ],
+        );
+
+        $asyncApi = Yaml::parseFile(codecept_data_dir('api/asyncapi/asyncapi.yml'));
+
+        // Assert
+        $this->tester->assertMessageExistsOnlyOnceInChannel($asyncApi, 'OutgoingMessage', 'channelNameA', 'subscribe');
+    }
+
+    /**
+     * This test ensures that the array 'oneOf' is not created unnecessarily if it still doesn't exist and the message is not added to the channel.
+     *
+     * Test Coverage: AsyncApiBuilder::addMessageToChannelType()
+     * if ($this->messageNameExists($messageName, $messages) === true) {
+     *     return $asyncApi;
+     * }
+     *
+     * @return void
+     */
+    public function testAddExistingMessageToChannelWithOneMessageDoesNotCreateOneOfArray(): void
+    {
+        // Arrange
+        $commandTester = $this->tester->getConsoleTester(SchemaMessageAddConsole::class, false);
+
+        // Act
+        // The message 'PaymentMethodAdded' is added for the second time in the channel 'payment'
+        $commandTester->execute(
+            [
+                '--' . SchemaMessageAddConsole::OPTION_MESSAGE_TYPE => 'publish',
+                '--' . SchemaMessageAddConsole::OPTION_PROPERTY => ['property:string'],
+                '--' . SchemaMessageAddConsole::OPTION_ASYNC_API_FILE => codecept_data_dir('api/asyncapi/asyncapi-one-reference.yml'),
+                SchemaMessageAddConsole::ARGUMENT_CHANNEL_NAME => 'payment',
+                SchemaMessageAddConsole::ARGUMENT_OPERATION_ID => 'operationId',
+                SchemaMessageAddConsole::ARGUMENT_MESSAGE_NAME => 'PaymentMethodAdded',
+            ],
+        );
+
+        $asyncApi = Yaml::parseFile(codecept_data_dir('api/asyncapi/asyncapi-one-reference.yml'));
+
+        // Assert
+        // The 'oneOf' array wasn't created
+        $this->assertTrue(!isset($asyncApi['channels']['payment']['publish']['message']['oneOf']));
     }
 }
